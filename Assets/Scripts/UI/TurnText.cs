1a62b77f2222e3467fd39889c49a417b59fa59fb
Kamil Jarząb <jarzbkamil@gmail.com>
2021-03-05 18:37:46 +0100

    Added turn indicator

---
+++ Assets/Scripts/UI/TurnText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnText : MonoBehaviour
+{
+    private UnityEngine.UI.Text txt;
+    void Start()
+    {
+        txt = GetComponent<UnityEngine.UI.Text>();
+
+    }
+
+    void Update()
+    {
+        txt.text = "Turn: " + Turn.GetUnitTurn();
+    }
+}
